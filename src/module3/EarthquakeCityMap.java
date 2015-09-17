d6398290e4c4a191478d4e814f0ec80362b495ba
Sylvain Leroux <sylvain@chicoree.fr>
2015-09-17 14:26:34 +0200

    Week 2 project

--- src/module3/EarthquakeCityMap.java
+++ src/module3/EarthquakeCityMap.java
@@ -2,17 +2,17 @@ package module3;
 
 //Java utilities libraries
 import java.util.ArrayList;
-//import java.util.Collections;
-//import java.util.Comparator;
 import java.util.List;
 
 //Processing library
 import processing.core.PApplet;
-
+import processing.core.PGraphics;
 //Unfolding libraries
 import de.fhpotsdam.unfolding.UnfoldingMap;
 import de.fhpotsdam.unfolding.marker.Marker;
 import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.events.MapEventBroadcaster;
+import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 import de.fhpotsdam.unfolding.providers.Google;
 import de.fhpotsdam.unfolding.providers.MBTilesMapProvider;
@@ -20,6 +20,42 @@ import de.fhpotsdam.unfolding.utils.MapUtils;
 
 //Parsing library
 import parsing.ParseFeed;
+
+class EarthquakeMarker extends SimplePointMarker {
+    final int radius = 30;
+    final float magnitude;
+    static final int colors[] = {
+      /*  0 */ 0xFF5AFF05,
+      /*  1 */ 0xFF7AF005,
+      /*  2 */ 0xFFB1F005,
+      /*  3 */ 0xFFD0F005,
+      /*  4 */ 0xFFF0E005,
+      /*  5 */ 0xFFF09D05,
+      /*  6 */ 0xFFF07E05,
+      /*  7 */ 0xFFF06305,
+      /*  8 */ 0xFFF04005,
+      /*  9 */ 0xFFF02C05,
+      /* 10 */ 0xFFF00505
+    };
+    
+    public EarthquakeMarker(Location location, float magnitude) {
+      super(location);
+      
+      this.magnitude = magnitude;
+    }
+   
+    public void draw(PGraphics pg, float x, float y) {
+      pg.pushStyle();
+      pg.noStroke();
+      
+      float color = PApplet.map(magnitude, 0, 10, 0, 10);
+      pg.fill(colors[Math.round(color)],150);
+      pg.ellipse(x, y, radius, radius);
+      pg.fill(255, 150);
+      pg.ellipse(x, y, radius-10, radius-10);
+      pg.popStyle();
+    }
+  }
 
 /** EarthquakeCityMap
  * An application with an interactive map displaying earthquake data.
@@ -73,20 +109,22 @@ public class EarthquakeCityMap extends PApplet {
 	    //PointFeatures have a getLocation method
 	    List<PointFeature> earthquakes = ParseFeed.parseEarthquake(this, earthquakesURL);
 	    
-	    // These print statements show you (1) all of the relevant properties 
-	    // in the features, and (2) how to get one property and use it
-	    if (earthquakes.size() > 0) {
-	    	PointFeature f = earthquakes.get(0);
-	    	System.out.println(f.getProperties());
-	    	Object magObj = f.getProperty("magnitude");
-	    	float mag = Float.parseFloat(magObj.toString());
-	    	// PointFeatures also have a getLocation method
-	    }
 	    
-	    // Here is an example of how to use Processing's color method to generate 
-	    // an int that represents the color yellow.  
-	    int yellow = color(255, 255, 0);
-	    
+	    for (PointFeature pointFeature : earthquakes) {
+            System.out.println(pointFeature.getProperties());
+
+	        Object magObj = pointFeature.getProperty("magnitude");
+            float mag = Float.parseFloat(magObj.toString());
+            
+            Marker marker = new EarthquakeMarker(pointFeature.location, mag);
+            //Marker marker = new SimplePointMarker(pointFeature.location);
+            //marker.setColor(color(255,0,0));
+
+            markers.add(marker);
+            
+            map.addMarker(marker);
+            
+        }
 	    //TODO: Add code here as appropriate
 	}
 		
@@ -100,7 +138,7 @@ public class EarthquakeCityMap extends PApplet {
 	}
 	
 	public void draw() {
-	    background(10);
+	    background(127);
 	    map.draw();
 	    addKey();
 	}
@@ -110,7 +148,15 @@ public class EarthquakeCityMap extends PApplet {
 	// TODO: Implement this method to draw the key
 	private void addKey() 
 	{	
+	    fill(255,255,255);
+	    rect(50,50,100,300);
 		// Remember you can use Processing's graphics methods here
-	
+	    for (int i = 0; i < EarthquakeMarker.colors.length; i++) {
+            int color = EarthquakeMarker.colors[i];
+            
+            fill(color);
+            rect(70,70+20*i,40,20);
+            text(Integer.toString(i), 120, 85+20*i);
+        }
 	}
 }
